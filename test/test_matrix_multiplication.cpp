d6d7ff7d67c8b597ad1ca2d2cd00e59972051d5a
Denis Untila <denis.untila@mail.polimi.it>
2024-05-29 11:53:12 +0000

    changed lib object file

--- test/test_matrix_multiplication.cpp
+++ test/test_matrix_multiplication.cpp
@@ -1,4 +1,5 @@
 #include "matrix_multiplication.h"
+#include "../src/matrix_mult.cpp"
 #include <iostream>
 #include <vector>
 #include <gtest/gtest.h>
@@ -6,6 +7,7 @@
 // ######################### Source code of multiplyMatrices in src/matrix_mult
 
 
+/*
 TEST(MatrixMultiplicationTest, TestMultiplyMatrices) {
     std::vector<std::vector<int>> A = {
         {1, 2, 3},
@@ -27,6 +29,39 @@ TEST(MatrixMultiplicationTest, TestMultiplyMatrices) {
 
     ASSERT_EQ(C, expected) << "Matrix multiplication test failed! :(((()";
 }
+*/
+
+
+TEST(MatrixMultiplicationTest, TestMultiplyMatrices) {
+    std::vector<std::vector<int>> A = {
+        {1, 2},
+        {4, 5}
+    };
+    std::vector<std::vector<int>> B = {
+        {7, 8},
+        {9, 10}
+    };
+    std::vector<std::vector<int>> C(2, std::vector<int>(2, 0));
+
+    multiplyMatrices(A, B, C, 2, 2, 2);
+
+    /*
+    std::vector<std::vector<int>> expected = {
+        {58, 64},
+        {139, 154}
+    };
+    */
+
+    std::vector<std::vector<int>> expected(2, std::vector<int>(2, 0)) /*= {
+        {25, 28},
+        {73, 82}
+    }*/;
+    multiplyMatricesWithoutErrors(A, B, expected, 2, 2, 2);
+
+    ASSERT_EQ(C, expected) << "Matrix multiplication test failed! :(((()";
+}
+
+
 
 int main(int argc, char **argv) {
     testing::InitGoogleTest(&argc, argv);
