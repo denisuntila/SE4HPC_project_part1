2c08a49f3331bba0fe16fc53bb51029f159e0879
Lucacapoferri <luca2.capoferri@mail.polimi.it>
2024-05-30 16:43:15 +0000

    capturing cerr buffer

--- test/test_matrix_multiplication.cpp
+++ test/test_matrix_multiplication.cpp
@@ -43,31 +43,35 @@ TEST(MatrixMultiplicationTest, TestUnitaryMatrices){
     std::vector<std::vector<int>> C(2,std::vector<int>(2, 0));
     std::vector<std::vector<int>> expected(2,std::vector<int>(2, 0));
 
-    for(int i=17; i < 18 ; i++){
+    for(int i=-10 ; i < 10 ; i++){
         std::stringstream buffer;
         std::streambuf *sbuf = std::cerr.rdbuf();
         std::cerr.rdbuf(buffer.rdbuf());
-        A1[0][0]=1;
+        A1[0][0]=i;
         B1[0][0]=i;
-        //A = {{i,i},{i,i}};
-        //B = {{i,i},{i,i}};
+        A = {{i,i},{i,i}};
+        B = {{i,i},{i,i}};
         multiplyMatrices(A1, B1, C1, 1,1,1);
+        
+        multiplyMatrices(A, B, C, 2, 2, 2);
         std::cerr.rdbuf(sbuf);
         std::cout << buffer.str()<<std::endl;
-        //multiplyMatrices(A, B, C, 2, 2, 2);
         multiplyMatricesWithoutErrors(A1, B1, expected1, 1,1,1);
-        //multiplyMatricesWithoutErrors(A, B, expected, 2, 2, 2);
+        multiplyMatricesWithoutErrors(A, B, expected, 2, 2, 2);
         EXPECT_EQ(C1, expected1) << "Matrix multiplication test failed! :(((()";
-        //EXPECT_EQ(C, expected) << "Matrix multiplication test failed! :(((()";
+        EXPECT_EQ(C, expected) << "Matrix multiplication test failed! :(((()";
     }
 }
 
-/*TEST(MatrixMultiplicationTest, TestRectangularMatrices){
+TEST(MatrixMultiplicationTest, TestRectangularMatrices){
     std::random_device rd;  // Seed for the random number engine
     std::mt19937 gen(rd()); // Mersenne Twister random number engine
     std::uniform_int_distribution<> dis(-1000, 1000);
 
     for(int i = 1; i <= 5; i++){
+        std::stringstream buffer;
+        std::streambuf *sbuf = std::cerr.rdbuf();
+        std::cerr.rdbuf(buffer.rdbuf());
         std::vector<std::vector<int>> A(3,std::vector<int>(i, 0));
         std::vector<std::vector<int>> B(i,std::vector<int>(3, 0));
         std::vector<std::vector<int>> C(3,std::vector<int>(3, 0));
@@ -81,12 +85,14 @@ TEST(MatrixMultiplicationTest, TestUnitaryMatrices){
         }
 
         multiplyMatrices(A, B, C, 3, i, 3);
+        std::cerr.rdbuf(sbuf);
+        std::cout << buffer.str()<<std::endl;
         std::vector<std::vector<int>> expected(3, std::vector<int>(3, 0));
         multiplyMatricesWithoutErrors(A, B, expected, 3, i, 3);
         EXPECT_EQ(C, expected) << "Matrix multiplication test failed! :(((()";
     }
-}*/
-/*
+}
+
 TEST(MatrixMultiplicationTest, TestOutputMatrices){
     std::vector<std::vector<int>> A(10,std::vector<int>(10, 0));
     std::vector<std::vector<int>> B(10,std::vector<int>(10, 0));
@@ -94,25 +100,34 @@ TEST(MatrixMultiplicationTest, TestOutputMatrices){
     std::vector<std::vector<int>> expected(10,std::vector<int>(10, 0));
     int count =0;
 
+    std::stringstream buffer;
+    std::streambuf *sbuf = std::cerr.rdbuf();
+    std::cerr.rdbuf(buffer.rdbuf());
+
     for(int i=0; i < 10 ; i++){
-        A[i][i]=1;
-        B[i][1] = 17;
+            A[i][i]=1;
+       
         for(int j=0;j<10;j++){
-            
+            B[i][j] = count;
             count++;
         }
     }
     multiplyMatrices(A, B, C, 10, 10, 10);
+    std::cerr.rdbuf(sbuf);
+    std::cout << buffer.str()<<std::endl;
     multiplyMatricesWithoutErrors(A, B, expected, 10, 10, 10);
     EXPECT_EQ(C, expected) << "Matrix multiplication test failed! :(((()";
-}*/
+}
 
-/*TEST(MatrixMultiplicationTest, TestRectangularMatrices2){
+TEST(MatrixMultiplicationTest, TestRectangularMatrices2){
     std::random_device rd;  // Seed for the random number engine
     std::mt19937 gen(rd()); // Mersenne Twister random number engine
     std::uniform_int_distribution<> dis(-15, 15);
 
     for(int i = 1; i <= 5; i++){
+        std::stringstream buffer;
+        std::streambuf *sbuf = std::cerr.rdbuf();
+        std::cerr.rdbuf(buffer.rdbuf());
         std::vector<std::vector<int>> A(i,std::vector<int>(3, 0));
         std::vector<std::vector<int>> B(3,std::vector<int>(i, 0));
         std::vector<std::vector<int>> C(i,std::vector<int>(i, 0));
@@ -126,11 +141,13 @@ TEST(MatrixMultiplicationTest, TestOutputMatrices){
         }
 
         multiplyMatrices(A, B, C, i, 3, i);
+        std::cerr.rdbuf(sbuf);
+        std::cout << buffer.str()<<std::endl;
         std::vector<std::vector<int>> expected(i, std::vector<int>(i, 0));
         multiplyMatricesWithoutErrors(A, B, expected, i, 3, i);
         EXPECT_EQ(C, expected) << "Matrix multiplication test failed! :(((()";
     }
-}*/
+}
 
 int main(int argc, char **argv) {
     testing::InitGoogleTest(&argc, argv);
