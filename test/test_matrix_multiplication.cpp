41c13d6e02ba69ff12e0493ee5f25d92c8622acc
ari <arianna.procaccio@mail.polimi.it>
2024-05-30 09:35:54 +0000

    modified tests

--- test/test_matrix_multiplication.cpp
+++ test/test_matrix_multiplication.cpp
@@ -32,54 +32,10 @@ TEST(MatrixMultiplicationTest, TestMultiplyMatrices) {
 }*/
 
 
-/*
-TEST(MatrixMultiplicationTest, TestMultiplyMatrices) {
-    std::vector<std::vector<int>> A = {
-        {1, 2},
-        {4, 5}
-    };
-    std::vector<std::vector<int>> B = {
-        {7, 8},
-        {9, 10}
-    };
-    std::vector<std::vector<int>> C(2, std::vector<int>(2, 0));
-
-    multiplyMatrices(A, B, C, 2, 2, 2);
-
-    /*
-    std::vector<std::vector<int>> expected = {
-        {58, 64},
-        {139, 154}
-    };
-    */
-
-/*    std::vector<std::vector<int>> expected(2, std::vector<int>(2, 0)) /*= {
-        {25, 28},
-        {73, 82}
-    }*/;
-    /*multiplyMatricesWithoutErrors(A, B, expected, 2, 2, 2);
-
-    ASSERT_EQ(C, expected) << "Matrix multiplication test failed! :(((()";
-}*/
-
-TEST(MatrixMultiplicationTest, TestMultiplyMatrices){
+TEST(MatrixMultiplicationTest, TestIncreasingRandomMatrices){
     std::random_device rd;  // Seed for the random number engine
     std::mt19937 gen(rd()); // Mersenne Twister random number engine
     std::uniform_int_distribution<> dis(-15, 15);
-
-    std::vector<std::vector<int>> A1(1,std::vector<int>(1, 0));
-    std::vector<std::vector<int>> B1(1,std::vector<int>(1, 0));
-    std::vector<std::vector<int>> C1(1,std::vector<int>(1, 0));
-    std::vector<std::vector<int>> expected1(1, std::vector<int>(1, 0));
-
-
-    for(int i=0; i < 10 ; i++){
-        A1[0][0]=i;
-        B1[0][0]=i;
-        multiplyMatrices(A1, B1, C1, 1,1,1);
-        multiplyMatricesWithoutErrors(A1, B1, expected1, 1,1,1);
-        ASSERT_EQ(C1, expected1) << "Matrix multiplication test failed! :(((()";
-    }
 
     for(int i =5; i < 50; i += 5){
         std::vector<std::vector<int>> A(i,std::vector<int>(i, 0));
@@ -96,7 +52,23 @@ TEST(MatrixMultiplicationTest, TestMultiplyMatrices){
         multiplyMatrices(A, B, C, i, i, i);
         std::vector<std::vector<int>> expected(i, std::vector<int>(i, 0));
         multiplyMatricesWithoutErrors(A, B, expected, i, i, i);
-        ASSERT_EQ(C, expected) << "Matrix multiplication test failed! :(((()";
+        EXPECT_EQ(C, expected) << "Matrix multiplication test failed! :(((()";
+    }
+}
+
+
+TEST(MatrixMultiplicationTest, TestUnitaryMatrices){
+    std::vector<std::vector<int>> A1(1,std::vector<int>(1, 0));
+    std::vector<std::vector<int>> B1(1,std::vector<int>(1, 0));
+    std::vector<std::vector<int>> C1(1,std::vector<int>(1, 0));
+    std::vector<std::vector<int>> expected1(1, std::vector<int>(1, 0));
+
+    for(int i=0; i < 10 ; i++){
+        A1[0][0]=i;
+        B1[0][0]=i;
+        multiplyMatrices(A1, B1, C1, 1,1,1);
+        multiplyMatricesWithoutErrors(A1, B1, expected1, 1,1,1);
+        EXPECT_EQ(C1, expected1) << "Matrix multiplication test failed! :(((()";
     }
 }
 
