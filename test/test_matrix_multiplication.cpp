8a4288336725f9a59adc6713ff0760743720bf13
ari <arianna.procaccio@mail.polimi.it>
2024-05-30 14:43:34 +0000

    modified test

--- test/test_matrix_multiplication.cpp
+++ test/test_matrix_multiplication.cpp
@@ -32,38 +32,13 @@ TEST(MatrixMultiplicationTest, TestMultiplyMatrices) {
 }*/
 
 
-TEST(MatrixMultiplicationTest, TestIncreasingRandomMatrices){
-    std::random_device rd;  // Seed for the random number engine
-    std::mt19937 gen(rd()); // Mersenne Twister random number engine
-    std::uniform_int_distribution<> dis(-15, 15);
-
-    for(int i =5; i < 50; i += 5){
-        std::vector<std::vector<int>> A(i,std::vector<int>(i, 0));
-        std::vector<std::vector<int>> B(i,std::vector<int>(i, 0));
-        std::vector<std::vector<int>> C(i,std::vector<int>(i, 0));
-
-        for(int r = 0; r < i; r++){
-            for(int c = 0; c < i; c++){   
-                A[r][c] = dis(gen);
-                B[r][c] = dis(gen);
-            }
-        }
-
-        multiplyMatrices(A, B, C, i, i, i);
-        std::vector<std::vector<int>> expected(i, std::vector<int>(i, 0));
-        multiplyMatricesWithoutErrors(A, B, expected, i, i, i);
-        EXPECT_EQ(C, expected) << "Matrix multiplication test failed! :(((()";
-    }
-}
-
-
 TEST(MatrixMultiplicationTest, TestUnitaryMatrices){
     std::vector<std::vector<int>> A1(1,std::vector<int>(1, 0));
     std::vector<std::vector<int>> B1(1,std::vector<int>(1, 0));
     std::vector<std::vector<int>> C1(1,std::vector<int>(1, 0));
     std::vector<std::vector<int>> expected1(1, std::vector<int>(1, 0));
 
-    for(int i=0; i < 10 ; i++){
+    for(int i=-10; i < 10 ; i++){
         A1[0][0]=i;
         B1[0][0]=i;
         multiplyMatrices(A1, B1, C1, 1,1,1);
@@ -72,7 +47,55 @@ TEST(MatrixMultiplicationTest, TestUnitaryMatrices){
     }
 }
 
+TEST(MatrixMultiplicationTest, TestRectangularMatrices){
+    std::random_device rd;  // Seed for the random number engine
+    std::mt19937 gen(rd()); // Mersenne Twister random number engine
+    std::uniform_int_distribution<> dis(-1000, 1000);
 
+    for(int i = 1; i <= 5; i++){
+        std::vector<std::vector<int>> A(3,std::vector<int>(i, 0));
+        std::vector<std::vector<int>> B(i,std::vector<int>(3, 0));
+        std::vector<std::vector<int>> C(3,std::vector<int>(3, 0));
+        //std:: cout << A.size() << " "<< B.size()<<std::endl;
+
+        for(int r = 0; r < 3; r++){
+            for(int c = 0; c < i; c++){   
+                A[r][c] = dis(gen);
+                B[c][r] = dis(gen);
+            }
+        }
+
+        multiplyMatrices(A, B, C, 3, i, 3);
+        std::vector<std::vector<int>> expected(3, std::vector<int>(3, 0));
+        multiplyMatricesWithoutErrors(A, B, expected, 3, i, 3);
+        EXPECT_EQ(C, expected) << "Matrix multiplication test failed! :(((()";
+    }
+}
+
+TEST(MatrixMultiplicationTest, TestRectangularMatrices2){
+    std::random_device rd;  // Seed for the random number engine
+    std::mt19937 gen(rd()); // Mersenne Twister random number engine
+    std::uniform_int_distribution<> dis(-15, 15);
+
+    for(int i = 1; i <= 5; i++){
+        std::vector<std::vector<int>> A(i,std::vector<int>(3, 0));
+        std::vector<std::vector<int>> B(3,std::vector<int>(i, 0));
+        std::vector<std::vector<int>> C(i,std::vector<int>(i, 0));
+        //std:: cout << A.size() << " "<< B.size()<<std::endl;
+
+        for(int r = 0; r < i; r++){
+            for(int c = 0; c < 3; c++){   
+                A[r][c] = dis(gen);
+                B[c][r] = dis(gen);
+            }
+        }
+
+        multiplyMatrices(A, B, C, i, 3, i);
+        std::vector<std::vector<int>> expected(i, std::vector<int>(i, 0));
+        multiplyMatricesWithoutErrors(A, B, expected, i, 3, i);
+        EXPECT_EQ(C, expected) << "Matrix multiplication test failed! :(((()";
+    }
+}
 
 int main(int argc, char **argv) {
     testing::InitGoogleTest(&argc, argv);
