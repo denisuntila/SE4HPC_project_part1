25a66c9e2bd2b41a019015fbbabdff70ea1c99d1
Lucacapoferri <luca2.capoferri@mail.polimi.it>
2024-05-31 13:51:49 +0000

    rdbuff 2

--- test/test_matrix_multiplication.cpp
+++ test/test_matrix_multiplication.cpp
@@ -8,28 +8,33 @@
 // ######################### Source code of multiplyMatrices in src/matrix_mult
 
 
-/*
+
 TEST(MatrixMultiplicationTest, TestMultiplyMatrices) {
+    std::vector<std::vector<int>> expected(3,std::vector<int>(2, 0));
     std::vector<std::vector<int>> A = {
-        {1, 2, 3},
-        {4, 5, 6}
+        {0, 1, 2, 0 },
+        {0, 0, 1, 2},
+        {3, 0, 1, 0}
     };
     std::vector<std::vector<int>> B = {
-        {7, 8},
-        {9, 10},
-        {11, 12}
+        {2, 8},
+        {3, 8},
+        {5, 1},
+        {2, 1}
     };
-    std::vector<std::vector<int>> C(2, std::vector<int>(2, 0));
+    std::vector<std::vector<int>> C(3, std::vector<int>(2, 0));
 
-    multiplyMatrices(A, B, C, 2, 3, 2);
+    multiplyMatrices(A, B, C, 3, 4, 2);
 
-    std::vector<std::vector<int>> expected = {
+    /*std::vector<std::vector<int>> expected = {
         {58, 64},
         {139, 154}
-    };
+    };*/
+
+    multiplyMatricesWithoutErrors(A, B, expected, 3,4,2);
 
     ASSERT_EQ(C, expected) << "Matrix multiplication test failed! :(((()";
-}*/
+}
 
 
 TEST(MatrixMultiplicationTest, TestUnitaryMatrices){
